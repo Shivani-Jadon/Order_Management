2b42100de597837e9f1f28503bcbea0e583e9a3b
Shivani Jadon <shivanijadon98@gmail.com>
2020-08-20 18:47:32 +0530

    add dependencies and create setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const expressHandlebars = require('express-handlebars');
+
+// listening to the server at port
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in starting server : ${err}`);
+    }
+
+    console.log(`Server started at port : ${port}`);
+});
