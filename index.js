8d47a21cdcd3a10ae0f01c2a25f3ab89ad0d6c0e
Shivani Jadon <shivanijadon98@gmail.com>
2020-08-20 19:25:20 +0530

    add home page view

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 9000;
 const expressHandlebars = require('express-handlebars');
+
+// configuring handlebar as our template engine
+app.engine('hbs', expressHandlebars({
+    defaultLayout: 'main',
+    extname : '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+//loading router
+app.use('/', require("./routes"));
+
 
 // listening to the server at port
 app.listen(port, function(err){
